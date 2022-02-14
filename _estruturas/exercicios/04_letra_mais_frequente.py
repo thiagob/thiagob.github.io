62ea98bd93027ac4b4979d5b14e103012c588813
Thiago Bohn <thiagob@gmail.com>
2022-02-14 18:27:06 -0300

    Create 04_letra_mais_frequente.py

---
+++ _estruturas/exercicios/04_letra_mais_frequente.py
@@ -0,0 +1,15 @@
+---
+layout: page
+title: Letra mais frequente
+category: exercicio
+subject: Python
+---
+Desenvolver uma rotina que percorra uma string e imprima¹qual é a letra mais frequente e a quantidade de vezes que foi encontrada.
+
+Exemplo
+
+    texto = "agua mole, pedra dura, tanto bate ate que fura"
+    # 'a'
+    # 8 vezes
+    
+¹ print()
