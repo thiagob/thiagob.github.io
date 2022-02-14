42f903bde5d65a5e50f4e4ad3d28b82f6cbdf8ca
Thiago Bohn <thiagob@gmail.com>
2022-02-14 18:21:50 -0300

    Create 03_adicionar_espaços.py

---
+++ _estruturas/exercicios/03_adicionar_espaços.py
@@ -0,0 +1,10 @@
+---
+layout: page
+title: Adicionar espaços
+category: exercicio
+subject: Python
+---
+Definir um método que receba uma string como parâmetro e retorne outra string, adicionando espaços entre cada caracter.
+
+    entrada = "thiago bohn"
+    saida = "t h i a g o   b o h n"
