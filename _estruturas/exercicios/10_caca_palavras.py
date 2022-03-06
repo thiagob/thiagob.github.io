58b4b0db0b680bfbc12908393b7291f91d1e9451
Thiago Bohn <thiagob@gmail.com>
2022-03-06 18:57:49 -0300

    Create 10_caca_palavras.py

---
+++ _estruturas/exercicios/10_caca_palavras.py
@@ -0,0 +1,22 @@
+---
+layout: page
+title: Caça-palavras
+category: exercicio
+subject: Python
+class_number: 3
+---
+Carregar o caça-palavras em uma matriz e percorrer linhas e colunas até encontrar a palavra Python.
+Ao encontrar a palavra, retornar a posição (linha e coluna) e a direção (se vertical ou horizontal).
+
+```
+XSXPKWPJMKSTLXRMRHBL
+KJTTZQCQSNDZBZQMZKKR
+FBTGILXBGCEHIBMKRXHS
+KGUJMJRQDIUDBOLCSNRZ
+ZZPKBPPLMUEXXAMDSGHZ
+MMUQNZYFPDOMTSJEPJZF
+DCDJXITPNKMROBMPUZIC
+PABYZJHARGTGFWNNGIUH
+HWEWWTOKCLHSWHMJHWBE
+ZASURRNKORXGIDYHYLRR
+```
