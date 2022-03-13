c7141a046679ed7779363d08eabc80fd24a9967a
Thiago Bohn <thiagob@gmail.com>
2022-03-13 13:58:17 -0300

    Create 12_fila.py

---
+++ _estruturas/exercicios/12_fila.py
@@ -0,0 +1,34 @@
+---
+layout: page
+title: Implementação Fila
+category: exercicio
+subject: Listas Lineares
+class_number: 4
+---
+Percorrer a matriz abaixo para identificar o maior número.
+
+```
+01, 20, 30, 01, 20, 02
+05, 12, 17, 18, 65, 53
+22, 10, 09, 45, 26, 12
+34, 22, 10, 14, 18, 22
+```
+
+### Exemplo de solução
+
+```python
+matriz = [
+    [1, 20, 30, 1, 20, 2],
+    [5, 12, 17, 18, 65, 53],
+    [22, 10, 9, 45, 26, 12],
+    [34, 22, 10, 14, 18, 22]
+]
+
+max = 0
+for linha in matriz:
+    for val in linha:
+        if val > max:
+            max = val
+
+print('Maior valor = ' + str(max))
+```
