ef10e4af50c2714841ff25edebe9a23b03a2c83c
Thiago Bohn <thiago.bohn@sap.com>
2022-04-18 21:21:36 -0300

    Exemplo de fibonacci feito em aula

--- _estruturas/exercicios/22_fibonacci.py
+++ _estruturas/exercicios/22_fibonacci.py
@@ -1,46 +1,66 @@
-def fibonacci_recursivo(numero):
-    if numero == 0:
-        return 0
-    elif numero == 1:
-        return 1
-    else:
-        return fibonacci_recursivo(numero - 1) + fibonacci_recursivo(numero - 2)
+def fib_recursivo(n):
 
-def fibonacci_iterativo(numero):
-    if numero <= 1:
-        return numero
-    elif numero == 2:
+    # condições de parada
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+
+    # F(n) = F(n - 1) + F(n - 2)
+    return fib_recursivo(n - 1) + fib_recursivo(n - 2)
+
+
+#print(fib_recursivo(0))
+#print(fib_recursivo(1))
+#print(fib_recursivo(2))
+
+#print(fib_recursivo(5))
+    # fib_recursivo(4)
+        # fib_recursivo(3)
+            # fib_recursivo(2)
+                # fib_recursivo(1)
+                # fib_recursivo(0)
+            # fib_recursivo(1)
+        # fib_recursivo(2)
+
+#print(fib_recursivo(8))
+
+
+def fib_iterativo(n):
+
+    if n == 0:
+        return 0
+    elif n == 1:
         return 1
     else:
-        a = 0
-        b = 1
-        c = 0
+        a = 0 # n - 2
+        b = 1 # n - 1
+        c = 0 # n
+
         i = 2
 
-        while i <= numero:
-            c = a + b
+        while (i <= n):
+            # F(n) = F(n - 1) + F(n - 2)
+            c = b + a
+
             a = b
             b = c
+
             i += 1
-        
+
         return c
 
-print('fib(0)')
-print(fibonacci_iterativo(0))
-print(fibonacci_recursivo(0))
 
-print('fib(2)')
-print(fibonacci_iterativo(2))
-print(fibonacci_recursivo(2))
+#print(fib_iterativo(0))
+#print(fib_iterativo(1))
 
-print('fib(5)')
-print(fibonacci_iterativo(5))
-print(fibonacci_recursivo(5))
+#print(fib_iterativo(2))
+#print(fib_iterativo(5))
+#print(fib_iterativo(6))
 
-print('fib(6)')
-print(fibonacci_iterativo(6))
-print(fibonacci_recursivo(6))
+print('Iterativo')
+print(fib_iterativo(35))
 
-print('fib(25)')
-print(fibonacci_iterativo(25))
-print(fibonacci_recursivo(25))
+print('Recursivo')
+print(fib_recursivo(35))
+
